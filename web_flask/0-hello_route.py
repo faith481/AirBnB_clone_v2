e5dc6b1fbda191a9daed3877a02f9437f3a0fe21
faith481 <faithayeni481@gmail.com>
2024-03-24 00:56:26 +0100

    Hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
+"""A script that starts a flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
     return "Hello HBNB!"
-
-
-@app.route("/hbnb")
-def hbnb():
-    return "HBNB"
 
 
 if __name__ == '__main__':
