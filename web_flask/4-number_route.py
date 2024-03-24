5ac630014115b41938e978507bcfecebd8cac236
faith481 <faithayeni481@gmail.com>
2024-03-24 16:36:08 -0700

    Is it a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,19 +10,19 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def home():
-        """The home page message"""
+    """The home page message"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hbnb_page():
-                """The HBNB page message"""
+    """The HBNB page message"""
     return "HBNB"
 
 
 @app.route("/c/<text>")
 def c_page(text):
-                        """The C page message displayed by the value of <text>"""
+    """The C page message displayed by the value of <text>"""
     text = text.replace("_", " ")
     return f"C {text}"
 
@@ -30,14 +30,14 @@ def c_page(text):
 @app.route("/python/<text>")
 @app.route("/python", defaults={"text": "is cool"})
 def python_page(text):
-                                    """The Python page displayed by the value of <text>"""
-text = text.replace("_", " ")
+    """The Python page displayed by the value of <text>"""
+    text = text.replace("_", " ")
     return f"Python {text}"
 
 
 @app.route("/number/<int:n>")
 def number_page(n):
-                                                """The number page displayed by n only if it is an integer"""
+    """The number page displayed by n only if it is an integer"""
     return f"{n} is a number"
 
 
