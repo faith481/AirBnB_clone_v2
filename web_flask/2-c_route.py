8bbfc6b23a8d359e35adb0b326ae2d9512c3ebf7
faith481 <faithayeni481@gmail.com>
2024-03-24 01:44:15 +0100

    Python is cool

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,29 +11,29 @@ app.url_map.strict_slashes = False
 @app.route("/")
 def home():
         """The home page message"""
-            return "Hello HBNB!"
+    return "Hello HBNB!"
 
 
-        @app.route("/hbnb")
-        def hbnb_page():
+@app.route("/hbnb")
+def hbnb_page():
                 """The HBNB page message"""
-                    return "HBNB"
+    return "HBNB"
 
 
-                @app.route("/c/<text>")
-                def c_page(text):
+@app.route("/c/<text>")
+def c_page(text):
                         """The C page message displayed by the value of <text>"""
-                            text = text.replace("_", " ")
-                                return f"C {text}"
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 
-                            @app.route("/python/<text>")
-                            @app.route("/python", defaults={"text": "is cool"})
-                            def python_page(text):
+@app.route("/python/<text>")
+@app.route("/python", defaults={"text": "is cool"})
+def python_page(text):
                                     """The Python page displayed by the value of <text>"""
-                                        text = text.replace("_", " ")
-                                            return f"Python {text}"
+text = text.replace("_", " ")
+return f"Python {text}"
 
 
-                                        if __name__ == "__main__":
-                                                app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
